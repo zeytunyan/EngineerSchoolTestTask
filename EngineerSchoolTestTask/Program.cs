949984806682ba475cc4d9a84ea5aff59f51baed
zeytunyan <georgiy.zeytunyan@gmial.com>
2023-05-24 01:23:14 +0300

    Сomplete Reflection task

--- EngineerSchoolTestTask/Program.cs
+++ EngineerSchoolTestTask/Program.cs
@@ -1,22 +1,46 @@
-﻿
-using EngineerSchoolTestTask;
+﻿using EngineerSchoolTestTask;
+using FrequencyDictionaryLib;
+using System.Reflection;
 
-var interactor = new ConsoleInteractor();
-
-interactor.Greetings = "Hello, this is a program for creating a frequency dictionary";
-interactor.AskEnterValueMessage = "Please, enter the file path:";
-interactor.FailMessage = "Failed to create a frequency dictionary";
-interactor.ActionDoneMessage = "The frequency dictionary was created";
+var interactor = new ConsoleInteractor
+{
+    Greetings = "Hello, this is a program for creating a frequency dictionary",
+    AskEnterValueMessage = "Please, enter the file path:",
+    FailMessage = "Failed to create a frequency dictionary",
+    ActionDoneMessage = "The frequency dictionary was created"
+};
 
 interactor.ContinuousInteraction(path =>
 {
-    var frequencyDictionary = new FrequencyDictionary(path ?? "");
+    var fileText = File.ReadAllText(path ?? "");
+
+    var methodname = "MakeWordFrequencyDictionaryFromText";
+    var frequencyDictionaryType = typeof(FrequencyDictionary);
+    var dictionaryMethod = frequencyDictionaryType.GetMethod(methodname, BindingFlags.NonPublic | BindingFlags.Instance);
+    var invokeResult = dictionaryMethod?.Invoke(new FrequencyDictionary(), new[] { fileText }) as Dictionary<string, int>;
+    Dictionary<string, int> frequencyDictionary = invokeResult ?? new(); 
+
     Console.WriteLine();
-    interactor.WriteEnumerable(frequencyDictionary);
+    interactor.WriteDictionary(frequencyDictionary);
     
-    if (interactor.AskYesOrNo("Save it to a file?"))
+    if (!interactor.AskYesOrNo("Save it to a file?"))
     {
-        var filePath = interactor.AskEnterValue() ?? "";
-        frequencyDictionary.SaveToFile(filePath);
+        return;
     }
+    
+    var filePath = interactor.AskEnterValue() ?? "";
+
+    using var writer = new StreamWriter(filePath, false);
+
+    if (!frequencyDictionary.Any())
+    {
+        writer.WriteLine("*No words found*");
+        return;
+    }
+
+    foreach (var wordFrequency in frequencyDictionary)
+    {
+        writer.WriteLine($"{wordFrequency.Key}: {wordFrequency.Value}");
+    }
+
 });
--- FrequencyDictionary/FrequencyDictionary.cs
+++ FrequencyDictionary/FrequencyDictionary.cs
@@ -2,12 +2,14 @@
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace EngineerSchoolTestTask
+namespace FrequencyDictionaryLib
 {
+    public record WordFrequency(string Word, int Frequency);
+
     public class FrequencyDictionary : IEnumerable<WordFrequency>
     {
         public Dictionary<string, int> WordFrequenciesDictionary { get; private set; } = new();
-        public WordFrequency[] WordFrequenciesArray { get; private set; } = { };
+        public WordFrequency[] WordFrequenciesArray { get; private set; } = Array.Empty<WordFrequency>();
 
         public FrequencyDictionary() { }
 
@@ -18,7 +20,7 @@ namespace EngineerSchoolTestTask
 
         public void AddDataFromFile(string filePath)
         {
-            using (StreamReader reader = new StreamReader(filePath))
+            using (var reader = new StreamReader(filePath))
             {
                 foreach (var fileWord in FileWords(reader))
                 {
@@ -46,6 +48,7 @@ namespace EngineerSchoolTestTask
             {
                 var symbolAsInt = reader.Read();
 
+                // Если файл закончился, возвращаем последнее слово и завершаем цикл
                 if (symbolAsInt == -1)
                 {
                     yield return word.ToString();
@@ -56,50 +59,71 @@ namespace EngineerSchoolTestTask
                 var isLetter = lettersRegex.Match(symbol).Success;
                 var isInWordSymbol = inWordSymbolsRegex.Match(symbol).Success;
 
+                // Если символ - буква
                 if (isLetter)
                 {
+                    // Если она идет после символа, который может быть внутри слова
                     if (inWordSymbol != "")
                     {
+                        // Добавляем этот символ и забываем его 
                         word.Append(inWordSymbol);
                         inWordSymbol = "";
                     }
 
+                    // Добавляем саму букву и идем дальше
                     word.Append(symbol);
                     continue;
                 }
 
+                // Если это символ, который может быть внутри слова,
+                // при этом он действительно внутри слова и перед ним нет другого такого
                 if (isInWordSymbol && word.Length > 0 && inWordSymbol == "")
                 {
+                    // Сохраняем его, чтобы добавить, если встретим букву, и идем дальше
                     inWordSymbol = symbol;
                     continue;
                 }
 
+                // Если это не буква и не символ, который может быть внутри слова
+                // или символ, который может быть внутри слова, но он не внутри
+                // или он там не один, то считаем, что слово готово
+
+                // Обнуляем предыдущий символ, который может быть внутри слова
                 inWordSymbol = "";
 
                 if (word.Length == 0) continue;
 
                 string madeWord = word.ToString();
                 word.Clear();
-                
+
                 yield return madeWord.ToLower();
             }
         }
 
         public void SaveToFile(string filePath)
         {
-            using (StreamWriter writer = new StreamWriter(filePath, false))
+            using var writer = new StreamWriter(filePath, false);
+            
+            if (Count == 0)
             {
-                if (Count == 0)
-                {
-                    writer.WriteLine("*No words found*");
-                    return;
-                }
-
-                foreach (var wordFrequency in this)
-                {
-                    writer.WriteLine($"{wordFrequency.Word}: {wordFrequency.Frequency}");
-                }
+                writer.WriteLine("*No words found*");
+                return;
             }
+
+            foreach (var wordFrequency in this)
+            {
+                writer.WriteLine($"{wordFrequency.Word}: {wordFrequency.Frequency}");
+            }
+        }
+
+        private Dictionary<string , int> MakeWordFrequencyDictionaryFromText(string text)
+        {
+            var wordRegex = new Regex("\\p{L}+([-‐‑‒–−—'’]?\\p{L}+)*");
+
+            return wordRegex.Matches(text)
+                .GroupBy(match => match.ToString().ToLower(), (word, matches) => new WordFrequency(word, matches.Count()))
+                .OrderByDescending(wordFrequency => wordFrequency.Frequency).ThenBy(wordFrequency => wordFrequency.Word)
+                .ToDictionary(wordFrequency => wordFrequency.Word, wordFrequency => wordFrequency.Frequency);
         }
 
         public int Count => WordFrequenciesArray.Length;
@@ -113,6 +137,4 @@ namespace EngineerSchoolTestTask
         
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
-
-    public record WordFrequency(string Word, int Frequency);
 }
